07f35e9863abc1116255cc9c3a47a6752ce64ef2
Grégoire <gregoire.riviere@kbrw.fr>
2023-08-15 18:18:15 +0200

    Fixing error

--- lib/token.ex
+++ lib/token.ex
@@ -1,6 +1,6 @@
 defmodule Token do
     
-    def base_path(), do: if System.get_env("DEPLOYED") == "TRUE", do: "/opt/link_me/", else: "/data/"
+    def base_path(), do: if System.get_env("DEPLOYED") == "TRUE", do: "/opt/link_me/", else: "data/"
 
     def secret(), do: if System.get_env("DEPLOYED") == "TRUE", do: "#{base_path()}/link_server", else: "#{base_path()}/link_server"
     def generate_secret(), do: File.write!("#{base_path()}/secret", :crypto.strong_rand_bytes(60) |> Base.encode64)
